0238df7ba3cba540dbed5b0746e6fa2261048e99
tranminhnhan102 <50911166+tranminhnhan102@users.noreply.github.com>
2019-07-17 00:42:54 +0700

    update

--- Auto/src/com/testfw/common/Constants.java
+++ Auto/src/com/testfw/common/Constants.java
@@ -1,10 +1,10 @@
 package com.testfw.common;
 
-import org.openqa.selenium.WebDriver;
+import com.testfw.driver.BaseDriver;
 
 public class Constants {
 	public static String URL = "https://www.foody.vn";
 	public static int ELEMENT_TIMEOUT = 10;
 	public static int ELEMENT_LONG_TIMEOUT = 60;
-	public static WebDriver DRIVER;
+	public static BaseDriver DRIVER;
 }
